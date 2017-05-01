c1c5668e1e27433d4331ca431ba03cef9c9841c4
DeadRisingCell <alexandrudinu99@yahoo.com>
2017-05-01 15:11:53 +0300

    Nothing special

---
+++ GameController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using Enemy;
+
+public class GameController : MonoBehaviour {
+	public static int enemyCount;
+
+	private	static GameObject[] enemies;
+	private static bool playerTurn = true;
+	private static int turnsCount = 1;
+
+	//public Text turnsCountText;
+
+	void Start () {
+			enemies = GameObject.FindGameObjectsWithTag("Enemy");
+			enemyCount = enemies.Length;
+
+			if(enemies == null){
+				Debug.LogWarning("No enemies in scene");
+			}
+			//turnsCountText.text = turnsCount.ToString();
+	}
+
+	
+
+	public void EndPlayerTurn () {
+		playerTurn = false;
+		turnsCount++;
+		//turnsCountText.text = turnsCount.ToString();
+		DoEnemiesActions();
+		BeginPlayerTurn();
+	}
+	public void BeginPlayerTurn () {
+		playerTurn = true;
+	}
+	public static bool ReturnPlayerTurn () {
+		return playerTurn;
+	}
+
+	public static int returnTurnsCount () {
+		return turnsCount;
+	}
+
+	public static void DoEnemiesActions () {
+			foreach(GameObject enemy in enemies){
+				if(enemy != null){
+					EnemyController _ec = enemy.GetComponent<EnemyController>() as EnemyController;
+					if(_ec != null) {
+						_ec.DoNextAction();
+					}
+				}
+			}
+	}
+
+	public static void RemoveEnemy (GameObject enemy) {
+		//update enemy list
+		for(int i=0; i<enemies.Length; i++){
+			if(enemies[i].Equals(enemy)){
+				enemies[i] = null;
+			}
+		}
+
+		foreach(GameObject en in enemies){
+			if(en != null){
+				Debug.Log(en.name);
+			}
+		}
+		//remove enemy from scene
+		Destroy(enemy);
+	}
+}
---
+++ PathFinding.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinding : MonoBehaviour {
+
+	public enum Directions {Forward, Right, Left, Backward};
+
+	public class Node {
+		int f, g, h;
+		bool walkable;
+	}
+
+	public int gridWorldSize = 21;
+
+	public Node[,] gridMap;
+
+	public List<Node> open_list;
+
+	public List<Node> closed_list;
+
+	void Start () {
+		gridMap = new Node[gridWorldSize, gridWorldSize];
+	}
+
+	public List<Directions> pathfinding(Vector3 start, Vector3 end){
+		
+	}
+
+	void Update () {
+		
+	}
+}
